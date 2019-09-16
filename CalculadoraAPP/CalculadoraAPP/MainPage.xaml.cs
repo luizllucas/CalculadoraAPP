235e4b32964dd43a4a814a6cbe312558ff5d4a7d
Luiz Lucas <cluizlucas@gmail.com>
2019-09-16 17:18:07 -0300

    Add project files.

---
+++ CalculadoraAPP/CalculadoraAPP/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalculadoraAPP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button7_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button6_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
