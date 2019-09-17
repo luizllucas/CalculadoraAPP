1ca3770f0c742dc56bcb9bd60cec573cdd2a9011
Luiz Lucas <cluizlucas@gmail.com>
2019-09-17 14:44:13 -0300

    Alterações App Calcullum

---
+++ CalculadoraAPP/CalculadoraAPP/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraAPP
+{
+   public class Calculadora
+    {
+       
+        public void Somar()
+        {
+            
+        }
+    }
+}
--- CalculadoraAPP/CalculadoraAPP/MainPage.xaml.cs
+++ CalculadoraAPP/CalculadoraAPP/MainPage.xaml.cs
@@ -7,21 +7,92 @@ using Xamarin.Forms;
 
 namespace CalculadoraAPP
 {
+    
     public partial class MainPage : ContentPage
     {
         public MainPage()
-        {
+        {           
             InitializeComponent();
         }
 
         private void Button7_Clicked(object sender, EventArgs e)
         {
+            entryValor.Text = "7";
+        }
 
+        private void Button8_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = "8";
+        }
+
+        private void Button9_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = "9";
+        }
+
+        private void ButtonSoma_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = "+";
         }
 
         private void Button6_Clicked(object sender, EventArgs e)
         {
+            entryValor.Text = "6";
+        }
 
+        private void Button5_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = "5";
+        }
+
+        private void Button4_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = "4";
+        }
+
+        private void ButtonMultiplicar_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = "*";
+        }
+
+        private void Button3_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = "3";
+        }
+
+        private void Button2_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = "2";
+        }
+
+        private void Button1_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = "1";
+        }
+
+        private void ButtonSubtrair_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = "-";
+        }
+
+        private void Button0_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = "0";
+        }
+
+        private void ButtonCE_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = "";
+        }
+
+        private void ButtonResultado_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = ;
+        }
+
+        private void ButtonDividir_Clicked(object sender, EventArgs e)
+        {
+            entryValor.Text = "/";
         }
     }
 }
